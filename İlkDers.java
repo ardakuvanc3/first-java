3ca42485b9c3fa5affebaf761669c7915e08508e
Arda Kuvanç <122490388+ardakuvanc3@users.noreply.github.com>
2023-07-25 20:23:05 +0300

    Add files via upload

---
+++ İlkDers.java
@@ -0,0 +1,33 @@
+/*public class ÝlkDers {
+  
+  public static void main(String[] args){
+    int interest = (1000*5)/100;
+    int fist_year = 1000;
+    int second_year = 1000+interest;
+    int third_year = 1000+interest*2;
+    System.out.println(fist_year);
+    System.out.println(second_year);
+    System.out.println(third_year);
+  }
+*/
+
+/*
+public class ÝlkDers {
+  
+  public static void main(String[] args){
+  
+  System.out.println("My Best Friend: Ada Saat");
+  System.out.println("My Best Friend's favorite movie: I don't know.");
+  System.out.print("My second friend: Arda Saat");
+  }}
+  
+ */
+
+import javax.swing.JOptionPane;
+public class ÝlkDers
+{
+ public static void main(String[] args)
+ {
+     JOptionPane.showMessageDialog(null, "Hello, World!");
+ }
+}
